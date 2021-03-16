87e63563b44c08a214056527a81467d35d8b39e0
Riley Slayden <slaydenriley@gmail.com>
2021-03-15 21:15:57 -0600

    Configured controller for logs"

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,2 +1,13 @@
 class LogsController < ApplicationController
+  def index
+    logs = Log.all
+    render json: logs
+  end
+
+  def create
+    if Log.create(logs: params[:logs])
+      logs = Log.all
+      render json: logs
+    end
+  end
 end
