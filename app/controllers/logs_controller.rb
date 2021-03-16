5225d1183facc5f38abced0b9bf80124f007bf9b
Riley Slayden <slaydenriley@gmail.com>
2021-03-16 11:28:57 -0600

    Changed order of logs to desc

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,12 +1,13 @@
 class LogsController < ApplicationController
   def index
-    logs = Log.all
+    logs = Log.all.order("created_at DESC")
     render json: logs
   end
 
   def create
     if Log.create(logs: params[:logs])
-      logs = Log.all
+      logs = Log.all.order("created_at DESC")
+      Log.first.destroy if Log.count > 10
       render json: logs
     end
   end
